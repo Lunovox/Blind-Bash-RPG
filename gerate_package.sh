b8d25f620e7f8822b9050e5301b44f554429de5a
Lunovox <lunovox@disroot.org>
2018-12-22 14:03:17 -0300

    Nova forma automatizada de criar novos pacotes DEB.

--- gerate_package.sh
+++ gerate_package.sh
@@ -3,7 +3,7 @@
 clear
 
 
-read -p "Digite o numero de versão (exemplo: 2.1.2):" version
+read -p "Digite o numero de versão (exemplo: 2.1.3): " version
 if [[ ${#version} -lt 3 ]]; then #onta caracteres
 	clear
 	echo "Versão '$version' não aceita!"
@@ -11,14 +11,14 @@ if [[ ${#version} -lt 3 ]]; then #onta caracteres
 	echo "	* <version_maior>...: Quantas vezes esse programa foi completamente recriado."
 	echo "	* <version_minor>...: Quantas recursos novos foram adicionados neste mesmo programa."
 	echo "	* <bug_fix>.........: Quantos bugs/defeitos foram corrigidos neste mesmo programa."
-	echo "Exemplo: '2.1.2'"
+	echo "Exemplo: '2.1.3'"
 	exit; 
 fi #Cancela a criação do pacote
-sed -i 's/<VERSION>/$version/' blind-bash-rpg/DEBIAN/control
-sed -i 's/<VERSION>/$version/' blind-bash-rpg/usr/bin/blind-bash-rpg
+sed -i "s/<VERSION>/$version/" blind-bash-rpg/DEBIAN/control
+sed -i "s/<VERSION>/$version/" blind-bash-rpg/usr/bin/blind-bash-rpg
 cd blind-bash-rpg/
 find usr/ -type f -exec md5sum {} \; > DEBIAN/md5sums
 cd ..
 dpkg-deb -b ./blind-bash-rpg/ ./Packages
-sed -i 's/$version/<VERSION>/' blind-bash-rpg/DEBIAN/control
-sed -i 's/<VERSION>/$version/' blind-bash-rpg/usr/bin/blind-bash-rpg
+sed -i "s/$version/<VERSION>/" blind-bash-rpg/DEBIAN/control
+sed -i "s/$version/<VERSION>/" blind-bash-rpg/usr/bin/blind-bash-rpg
